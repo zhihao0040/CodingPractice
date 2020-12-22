cc8ad9c3d1c09b143dfe785b07c667bcbbb0fba0
zhihao0040 <58205698+zhihao0040@users.noreply.github.com>
2020-12-23 10:38:14 +1100

    Started insertionSort.c

---
+++ sort/InsertionSort/insertionSort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv){
+    if (argc != 2){
+        printf("Please provide file name");
+        return 1;
+    }
+
+    // char* fileName = argv[1];
+
+    // FILE *fp = fopen(fileName, "r");
+
+    // int numOfElems;
+    // fscanf(fp, "%d\n", &numOfElems);
+
+    // char* myTextArray = malloc();
+
+    // Repeat Work! Let's make this a function.
+    
+}
+
+char* getFileDataIntoArray(char* fileName){
+    FILE *fp = fopen(fileName, "r");
+
+    int numOfElems;
+    fscanf(fp, "%d\n", &numOfElems);
+
+    char* myTextArray = malloc(sizeof(char) * numOfElems);
+    
+    // Fill up myTextArray
+    for (int i = 0; i < numOfElems; i++){
+        fscanf(fp, "%c\n", &myTextArray[i]);
+        // printf("%c\n", myTextArray[i]);
+    }
+
+    fclose(fp);
+
+    return myTextArray;
+}
